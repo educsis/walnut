37cf5fbd6ee84d05bede0e0f4909efa6f2f876d0
Edwin Orellana <educsis@gmail.com>
2025-01-23 12:53:50 -0600

    Randomly added a cell that contains the class for the squirell image

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@ const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 
 function createTheGrid() {
+    const numberCells = 5 * 5
+    const indexRnd = Math.floor(Math.random() * numberCells)
+
     grid.innerHTML = ''
     grid.style.gridTemplateColumns = `repeat(${5}, 50px)`
+    
     for (let i = 0; i < 5 * 5; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cell')
         grid.appendChild(cell)
+
+        if (i === indexRnd) {
+            cell.classList.add('squirell');
+        }
 
         // create the hover to change to yellow
         cell.addEventListener('mouseenter', () => {
