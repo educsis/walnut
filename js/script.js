ea34be05d7b4c41a01d83b70cf1a78ed63fa16d6
Edwin Orellana <educsis@gmail.com>
2025-01-23 20:16:25 -0600

    Added function that found the squirell, sum the yellow boxes and white boxes

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 
 function createTheGrid() {
+    // clear everything
+    document.getElementById('well-done').innerHTML = ''
+    document.getElementById('yellow-count').innerHTML = '0'
+    document.getElementById('white-count').innerHTML = '0'
+
     const gridType = document.getElementById('grid-select')
     const size = parseInt(gridType.value)
     const numberCells = size * size
     const indexRnd = Math.floor(Math.random() * numberCells)
+    const yellowCount = document.getElementById('yellow-count')
+    const whiteCount = document.getElementById('white-count')
+
+    let yeBoxes = 0
+    let whBoxes = 0
 
     grid.innerHTML = ''
     grid.style.gridTemplateColumns = `repeat(${size}, 50px)`
@@ -21,10 +31,30 @@ function createTheGrid() {
         }
 
         // create the hover to change to yellow
-        cell.addEventListener('mouseenter', () => {
+        cell.addEventListener('mouseenter', (e) => {
             cell.classList.toggle('yellow-cell')
+
+            let targetClass = e.target.className
+
+            if(targetClass === 'cell') {
+                yeBoxes +=  1
+                yellowCount.innerHTML = yeBoxes
+            } else if(targetClass == 'cell yellow-cell') {
+                whBoxes += 1
+                whiteCount.innerHTML = whBoxes
+            } else if(targetClass == 'cell squirell' || targetClass == 'cell squirell yellow-cell') {
+                foundSquirell()
+            }
         });
     }
+}
+
+function foundSquirell() {
+    start.disabled = false
+    stop.disabled = true
+    // clear the grid
+    grid.innerHTML = ''
+    document.getElementById('well-done').innerHTML = '<h1 class="text-color-4">Well Done! <br> You have found the squirell</h1>'
 }
 
 start.addEventListener('click', () => {
